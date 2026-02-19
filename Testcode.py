69f0b04e7e5b484bcfeac869449f9ad396892aad
Juan Camilo Marquez <camilomarquez.ja@academia.umb.edu.co>
2026-02-19 12:52:59 -0500

    Actualizo codigo trabajo 1

---
+++ Testcode.py
@@ -0,0 +1,7 @@
+l1=float(input("dame uno de los lados del cuadrado: "))
+area=l1*l1
+print("el area del cuadrado es: ", area)
+
+if area>100:
+    print("el area es mayor a 100") 
+else:    print("el area es menor a 100")
