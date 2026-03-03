e5f56c787f1ae77ef99d2a8e570f823b9dc4bc4c
Juan Camilo Marquez <camilomarquez.ja@academia.umb.edu.co>
2026-03-02 20:57:01 -0500

    While y for

---
+++ for_while.py
@@ -0,0 +1,20 @@
+
+rondas=int(input("Ingrese el número de rondas entre 3 y 6: "))
+while rondas<3 or rondas>6:
+    print("Debe ser un numero entre 3 y 6.")
+    rondas=int(input("Ingrese el número de rondas entre 3 y 6: "))
+for i in range(rondas):
+    print("Ronda", i+1)
+
+
+nnatural=int(input("Ingrese un número natural, el cual se hara la suma desde 1: "))
+for n in range(1, nnatural+1):
+    print(n)
+suma=nnatural*(nnatural+1)//2
+print("La suma de los números naturales desde 1 hasta", nnatural, "es:", suma)
+
+
+resultado_c=int(input("Ingrese el calculo que ud hizo: "))
+if resultado_c==suma:
+    print("El resultado es Correcto ud gano 10 puntos")
+else:    print("El resultado correcto es incorrecto")
