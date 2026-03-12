59ea8ccaf4baf114d00ea7aab8bfb6f5278c83ee
Juan Camilo Marquez <camilomarquez.ja@academia.umb.edu.co>
2026-03-12 10:26:11 -0500

    ultima

--- par_impar.py
+++ par_impar.py
@@ -1,5 +1,6 @@
 #par o impar
 numero = int(input("Ingrese un numero: "))
 if numero % 2 == 0:
-    print("El numero es par.")  
-else:    print("El numero es impar.")
+    print("es par.")  
+else:    print("impar.")
+
